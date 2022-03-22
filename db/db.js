7876d6b828d8081f92c14186bc0d6cdafcf82597
AntonKuksov <Anton.Kuksov@github.com>
2022-03-22 18:34:27 +0200

    Add my link

--- db/db.js
+++ db/db.js
@@ -3,20 +3,20 @@ export default {
         about: {
             text: [
                 "Hi &#128075;",
-                "I'm Anton Kuksov. I’m currently working as a software engineer@Novopay specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+                "I'm Anton Kuksov. I’m currently working as a software specialized in JavaScript Applications.",
                 "I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
                 "I love to read, listen to music, and travel.",
             ],
         },
         contact: {
             text: [
-                "If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+                "If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: antkuk123@gmail.com",
             ],
         },
     },
     skills: [{
             title: "Languages",
-            skillName: "JavaScript, C++",
+            skillName: "JavaScript, C#",
             color: "1",
             percentage: "70",
         },
@@ -28,15 +28,9 @@ export default {
         },
         {
             title: "Backend",
-            skillName: "Nodejs, MongoDB",
+            skillName: "Nodejs, OracleDB",
             color: "2",
             percentage: "40",
-        },
-        {
-            title: "Clouds",
-            skillName: "AWS(EC2, S3), Heroku, Netlify",
-            color: "3",
-            percentage: "30",
         },
         {
             title: "Design",
@@ -46,7 +40,7 @@ export default {
         },
         {
             title: "Version Control",
-            skillName: "GitHub, JIRA, Trello",
+            skillName: "GitHub, JIRA",
             color: "7",
             percentage: "70",
         },
@@ -55,12 +49,6 @@ export default {
             skillName: "Postman, Chrome DevTools",
             color: "3",
             percentage: "80",
-        },
-        {
-            title: "Saas products",
-            skillName: "CleverTap, FreshDesk",
-            color: "5",
-            percentage: "50",
         },
         {
             title: "Editor",
@@ -74,7 +62,7 @@ export default {
                 projectName: "Programming Diaries",
                 image: "images/programmingdiaries.png",
                 summary: "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-                preview: "https://programmingdiaries.herokuapp.com/",
+                //   preview: "https://programmingdiaries.herokuapp.com/",
                 techStack: [
                     "Django",
                     "SQLite",
@@ -87,21 +75,21 @@ export default {
                 projectName: "Find Your Bank",
                 image: "images/findyourbank.png",
                 summary: "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-                preview: "https://clever-fermi-0d5d76.netlify.app",
+                // preview: "https://clever-fermi-0d5d76.netlify.app",
                 techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
             },
             {
                 projectName: "Web Portfolio",
                 image: "images/portfolio.png",
                 summary: "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-                preview: "https://github.com/vinaysomawat/Travographer-Portal",
+                //  preview: "https://github.com/vinaysomawat/Travographer-Portal",
                 techStack: ["HTML", "Bootstrap", "JavaScript"],
             },
             {
                 projectName: "Resume Builder",
                 image: "images/resume-builder.png",
                 summary: "Browser based editor to build and download Resumes in a customizable templates.",
-                preview: "https://vinaysomawat.github.io/Resume-Builder",
+                //  preview: "https://vinaysomawat.github.io/Resume-Builder",
                 techStack: ["HTML", "Bootstrap", "JavaScript"],
             },
         ],
@@ -109,21 +97,21 @@ export default {
                 projectName: "Pizza Ordering ChatBot",
                 image: "images/pizzaorderchatbot.png",
                 summary: "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
-                preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+                // preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
                 techStack: ["Dailogflow", "Firebase"],
             },
             {
                 projectName: "WhatsApp-Bot",
                 image: "images/whatsappbot.jpg",
                 summary: "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-                preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+                // preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
                 techStack: ["Selenium", "Chrome Webdriver", "Python"],
             },
             {
                 projectName: "Bill Generator",
                 image: "images/billgenerator.png",
                 summary: "GUI to transfer data to excel sheets and generate bills on the local shops.",
-                preview: "https://github.com/vinaysomawat/Bill-Generator",
+                // preview: "https://github.com/vinaysomawat/Bill-Generator",
                 techStack: ["Tkinter", "Openxlpy", "Python"],
             },
         ],
@@ -131,14 +119,14 @@ export default {
                 projectName: "NITW-CSE",
                 image: "images/nitwcse.jpg",
                 summary: "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-                preview: "https://github.com/vinaysomawat/NITW-CSE",
+                //  preview: "https://github.com/vinaysomawat/NITW-CSE",
                 techStack: ["JAVA", "XML", "Android"],
             },
             {
                 projectName: "CareerHigh-App",
                 image: "images/carrerhigh.png",
                 summary: "The Application display the webpages of website careerhigh.in in android devices.",
-                preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+                // preview: "https://github.com/vinaysomawat/CareerHigh-Android",
                 techStack: ["JAVA", "XML", "Android"],
             },
         ],
@@ -146,14 +134,14 @@ export default {
                 projectName: "SnylloAir.com",
                 image: "images/snylloair.png",
                 summary: "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-                preview: "https://www.snylloair.com/",
+                //preview: "https://www.snylloair.com/",
                 techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
             },
             {
                 projectName: "Delivery+",
                 image: "images/AM-Logo-.png",
                 summary: "Android Application to display website in android devices.",
-                preview: "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+                //preview: "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
                 techStack: ["Android", "JAVA", "Play Store"],
             },
         ],
@@ -222,18 +210,9 @@ export default {
     footer: [{
             label: "Dev Profiles",
             data: [{
-                    text: "Stackoverflow",
-                    link: "https://stackoverflow.com/users/8461233/vinay-somawat",
-                },
-                {
-                    text: "GitHub",
-                    link: "https://github.com/vinaysomawat",
-                },
-                {
-                    text: "LeetCode",
-                    link: "https://leetcode.com/somawatvinay/",
-                },
-            ],
+                text: "GitHub",
+                link: "https://github.com/AntonKuksov",
+            }],
         },
         {
             label: "Resources",
@@ -244,28 +223,15 @@ export default {
                 {
                     text: "Print this page",
                     func: "window.print()",
-                },
-                {
-                    text: "Clone this page",
-                    link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
-                },
+                }
             ],
         },
         {
             label: "Social Profiles",
             data: [{
-                    text: "Linkedin",
-                    link: "https://www.linkedin.com/in/vinaysomawat/",
-                },
-                {
-                    text: "Twitter",
-                    link: "https://twitter.com/thesigmakid",
-                },
-                {
-                    text: "Buy me a coffee",
-                    link: "https://www.buymeacoffee.com/r194dME8y",
-                },
-            ],
+                text: "Linkedin",
+                link: "https://www.linkedin.com/in/anton-kuksov-43449b19a/",
+            }],
         },
         {
             label: "copyright-text",
