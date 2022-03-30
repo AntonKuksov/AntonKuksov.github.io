36b4993b040c841d9b1c5afbd46af5ddc9b03d76
AntonKuksov <Anton.Kuksov@github.com>
2022-03-30 23:13:29 +0300

    a

--- db/db.js
+++ db/db.js
@@ -139,21 +139,6 @@ export default {
                 techStack: ["Tkinter", "Openxlpy", "Python"],
             },
         ],
-        android: [{
-                projectName: "NITW-CSE",
-                image: "images/nitwcse.jpg",
-                summary: "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-                preview: "https://github.com/vinaysomawat/NITW-CSE",
-                techStack: ["JAVA", "XML", "Android"],
-            },
-            {
-                projectName: "CareerHigh-App",
-                image: "images/carrerhigh.png",
-                summary: "The Application display the webpages of website careerhigh.in in android devices.",
-                preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-                techStack: ["JAVA", "XML", "Android"],
-            },
-        ],
         freelance: [{
                 projectName: "SnylloAir.com",
                 image: "images/snylloair.png",
